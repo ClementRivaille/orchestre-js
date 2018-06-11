cfaeea1226aa6b4929cc2ec39ce5eb586ab9683d
Unknown <rivaille.clement@orange.fr>
2018-06-11 21:45:54 +0200

    feat: events and stop

--- demo/index.js
+++ demo/index.js
@@ -20,6 +20,7 @@ const orchestre = new Orchestre(115);
 orchestre.addPlayers(players).then(() => {
   orchestre.start();
   document.getElementById('control').className = '';
+  orchestre.onBeat(beat, 2, {interval: true})
 });
 
 window.chords = function() {
@@ -29,5 +30,15 @@ window.guitar = function() {
   orchestre.trigger('guitar', {fade: 1, now: true});
 }
 window.bass = function() {
-  orchestre.trigger('bass', {once: true});
+  orchestre.trigger('bass', {once: false});
+}
+
+function beat() {
+  const beatElem = document.getElementById('beat');
+  beatElem.className = beatElem.className === 'hidden' ? '' : 'hidden';
+}
+
+
+window.stop = function() {
+  orchestre.fullStop();
 }
--- src/orchestre.js
+++ src/orchestre.js
@@ -13,13 +13,66 @@ class Orchestre {
     this.eventEmitter = new EventEmitter();
     this.metronome = new Metronome(bpm, this.context, this.eventEmitter);
     this.loader = new BufferLoader(this.context);
+
+    this.subscribers = [];
+    this._updateEvents = this._updateEvents.bind(this);
+
+    this.started = false;
+  }
+
+  /**
+   * At each beat, call eventual subscribers
+   */
+  _updateEvents() {
+    if (this.subscribers.length > 0) {
+      const toRemove = [];
+      for (const sub of this.subscribers) {
+        // Decrease the number of beat to wait
+        sub.wait -= 1;
+        if (sub.wait <= 0) {
+          // Call subscriber function
+          try {sub.callback();}
+          catch(err) {throw new(err);}
+          finally {
+            if (sub.interval)
+              // Repeat if interval
+              sub.wait = sub.length;
+            else
+              toRemove.push(this.subscribers.indexOf(sub));
+          }
+        }
+      }
+      // Remove called subscribers
+      for (let i of toRemove) {
+        this.subscribers.splice(i, 1);
+      }
+    }
   }
 
   /** Start metronome */
   start() {
+    if (this.started) throw new Error('Orchestre is already started');
     this.context.resume();
     this.metronome.start(this.context.currentTime + 0.1);
+
+    this.eventEmitter.subscribe('beat', this._updateEvents);
+    this.started = true;
   }
+
+  /**
+  * Immediately stop all the instruments, then stop the metronome
+  */
+  fullStop() {
+    if (!this.started) throw new Error('Orchestre has not been started');
+    for (const player in this.players) {
+      if (this.players.hasOwnProperty(player))
+        this.players[player].soundLoop.stop(this.context.currentTime);
+    }
+    this.eventEmitter.unsubscribe('beat', this._updateEvents);
+    this.metronome.stop();
+    this.started = false;
+  }
+
 
   /**
   * Prepare sounds
@@ -39,7 +92,7 @@ class Orchestre {
   }
 
   /** Prepare a single sound */
-  addPlayer(name, url, length, absolute, destination) {
+  addPlayer(name, url, length, absolute=false, destination) {
     return this.loader.load(name, url).then(buffer => {
       this.players[name] = {
         name,
@@ -69,10 +122,10 @@ class Orchestre {
   *     * now (bool): if true, sound will start / stop immediately. Otherwise, it waits for next beat.
   *     * once (bool): play sound only once, then stop
   */
-  trigger(name, options) {
+  trigger(name, options={}) {
+    if (!this.started) throw new Error('Orchestre has not been started');
     let player = this.players[name];
     if (!player) throw new Error(`Player ${name} does not exist`);
-    options = options || {};
 
 
     if (!player.soundLoop.playing) {
@@ -94,10 +147,10 @@ class Orchestre {
   *     * now (bool): if true, sound will start immediately. Otherwise, it waits for next beat.
   *     * once (bool): play sound only once, then stop
   */
-  play(name, options) {
+  play(name, options={}) {
+    if (!this.started) throw new Error('Orchestre has not been started');
     let player = this.players[name];
     if (!player) throw new Error(`play: player ${name} does not exist`);
-    options = options || {};
     player.soundLoop.start(options.now ? this.context.currentTime : this.metronome.getNextBeatTime(), this.metronome, options.fade || 0, options.once);
   }
 
@@ -108,10 +161,10 @@ class Orchestre {
   *     * fade (float): time constant for fade out
   *     * now (bool): if true, sound will stop immediately. Otherwise, it waits for next beat.
   */
-  stop(name, options) {
+  stop(name, options={}) {
+    if (!this.started) throw new Error('Orchestre has not been started');
     let player = this.players[name];
     if (!player) throw new Error(`stop: player ${name} does not exist`);
-    options = options || {};
     player.soundLoop.stop(options.now ? this.context.currentTime : this.metronome.getNextBeatTime(), options.fade || 0);
   }
 
@@ -128,13 +181,12 @@ class Orchestre {
   *     * fade (float): time constant for fade in or fade out
   *     * once (bool): play sound only once, then stop
   */
-  schedule(name, beats, action, options) {
+  schedule(name, beats, action='trigger', options={}) {
+    if (!this.started) throw new Error('Orchestre has not been started');
     const player = this.players[name];
     if (!player) throw new Error(`schedule: player ${name} does not exist`);
     if (beats <= 0) throw new Error(`schedule: beats must be a positive number`);
-    options = options || {};
 
-    action = action || 'trigger';
     const eventTime = this.metronome.getNextNthBeatTime(beats);
     if (action === 'play' || (action === 'trigger' && !player.soundLoop.playing)) {
       player.soundLoop.start(eventTime, this.metronome, options.fade || 0, options.once);
@@ -145,6 +197,39 @@ class Orchestre {
     else {
       throw new Error(`schedule: action ${action} is not recognized (must be within ['play', 'stop', 'trigger'])`)
     }
+  }
+
+  /**
+  * Wait a number of beats before calling a function
+  * @param callback {function}
+  * @param nbBeats {number} (optional) number of beats to wait [default=1]
+  * @param options {objects}
+  *   * absolute (bool) Callback will be called on the next absolute Nth beat (like next measure)
+  *   * interval (bool) Callback will be called every n beats
+  *   * offset (number) Used with absolute to set a position in the measure
+  *
+  */
+  onBeat(callback, beats=1, options={}) {
+    this.subscribers.push({
+      callback,
+      length: beats,
+      interval: options.interval,
+      wait: beats -
+        (options.absolute ? this.metronome.getBeatPosition(this.context.currentTime, beats) : 0) +
+        (options.offset || 0)
+    });
+  }
+
+  /**
+  * Remove an existing interval
+  * @return true if found
+  */
+  removeInterval(callback) {
+    const subIndex = this.subscribers.findIndex((sub) => sub.callback === callback);
+    if (subIndex !== -1) {
+      this.subscribers.splice(subIndex, 1);
+    }
+    return subIndex !== -1;
   }
 }
 
